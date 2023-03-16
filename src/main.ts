a579de203bcc01f1b7c438b2144cbedf613d7ce6
StarHeartHunt <starheart233@gmail.com>
2023-03-16 11:20:16 +0800

    feat: add proxy middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+
+//import { createProxyMiddleware } from 'http-proxy-middleware';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /*
+  app.use(
+    '/proxy/jxfw/',
+    createProxyMiddleware({
+      target: 'https://jxfw.gdut.edu.cn/',
+      pathRewrite: {
+        '/proxy/jxfw/': '',
+      },
+      changeOrigin: true,
+      secure: false,
+    }),
+  );
+  */
   await app.listen(3000);
 }
 bootstrap();
