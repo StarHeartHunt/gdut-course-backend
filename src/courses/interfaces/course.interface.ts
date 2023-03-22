4659af0cea7ab283798fe3bd5351853727b027fe
StarHeartHunt <starheart233@gmail.com>
2023-03-22 11:10:27 +0800

    feat: expose all weeks to frontend

--- src/courses/interfaces/course.interface.ts
+++ src/courses/interfaces/course.interface.ts
@@ -5,9 +5,7 @@ export interface Course {
   teacher: string; // 老师
   startNode: number; // 开始为第几节课
   endNode: number; // 结束时为第几节课
-  startWeek: number; // 开始周
-  endWeek: number; // 结束周
-  type: number; // 单双周，每周为0，单周为1，双周为2
+  weeks: Array<number>; // 课程周次，如[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   credit: number; // 学分
   note: string; // 备注
   startTime: string; // 不规则的开始时间，长度必须为5，如"08:08"
